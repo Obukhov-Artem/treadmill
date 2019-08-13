7429f0474eadd2bba5970dc3e784c4b321cbb7c5
Андрей <45171006+ProGlot003@users.noreply.github.com>
2019-08-13 15:05:44 +0300

    Рабочая arduino

--- arduino_demo/arduino_demo.ino
+++ arduino_demo/arduino_demo.ino
@@ -1,17 +1,28 @@
+#define out1 2
+#define out2 3
+#define out3 5
+
 int symbol = 33;
 String txtMsg;
 String bill = "treadmill";
 String NewText;
 boolean stringComplete = false;
 int t = 0;
-
+int Speed = 0;
 void setup() {
   Serial.begin(115200);
+  digitalWrite(LED_BUILTIN, LOW);
   Serial.println("Loading");
+  pinMode(out1, OUTPUT);
+  pinMode(out2, OUTPUT);
+  pinMode(out3, OUTPUT);
   delay(1000);
 }
 
 void loop() {
+  if (txtMsg == 0){
+    digitalWrite(LED_BUILTIN, HIGH);
+    }
   if (t <= 1)
   {
     Serial.println(bill);
@@ -23,11 +34,16 @@ void loop() {
     if (stringComplete)
     {
       Serial.println(txtMsg);
+      Speed = txtMsg.toInt();
+      digitalWrite(out1, LOW);
+      digitalWrite(out2, HIGH);
+      analogWrite(out3, Speed);
       stringComplete = false;
       txtMsg = "";
     }
   }
 }
+
 char inChar;
 
 
@@ -36,7 +52,7 @@ void serialEvent()
   while (Serial.available())
   {
     char inChar = (char)Serial.read();
-    txtMsg += inChar;
     if (inChar == '.') stringComplete = true;
+    else txtMsg += inChar;
   }
 }
