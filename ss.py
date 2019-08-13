353dbef8baa31f6a5cb2d439151005e0a1016558
Андрей <45171006+ProGlot003@users.noreply.github.com>
2019-08-13 15:05:10 +0300

    Рабочая запись в csv

--- ss.py
+++ ss.py
@@ -5,21 +5,26 @@ import sys
 import glob
 import serial
 import time
+# from testSlider import data_in_arduino
 import csv
 import triad_openvr
 import threading
 
-CURRENT_ACTION = 0
+# print(data_in_arduino)
+CURRENT_ACTION = 1
+
+
+# CURRENT_ACTION = data_in_arduino
+
+
 class Get_data_trackers():
     def __init__(self):
         self.fieldnames = ['x_tracker_1', 'y_tracker_1', 'z_tracker_1',
-                      'x_tracker_2', 'y_tracker_2', 'z_tracker_2',
-                      'x_tracker_3', 'y_tracker_3', 'z_tracker_3',
-                      'x_tracker_4', 'y_tracker_4', 'z_tracker_4',
-                      'data_on_treadmill']
+                           'x_tracker_2', 'y_tracker_2', 'z_tracker_2',
+                           'x_tracker_3', 'y_tracker_3', 'z_tracker_3',
+                           'x_tracker_4', 'y_tracker_4', 'z_tracker_4',
+                           'data_on_treadmill']
         self.calibration()
-
-
 
     def csv_writer(self, path, fieldnames, data):
         with open(path, "w", newline='') as out_file:
@@ -30,7 +35,7 @@ class Get_data_trackers():
 
     def calibration(self):
         v = triad_openvr.triad_openvr()
-        print(v.devices)
+        #print(v.devices)
         right_hand = None
         right_knee = None
         right_leg = None
@@ -41,19 +46,22 @@ class Get_data_trackers():
             position_device = v.devices[device].sample(1, 500)
 
             if position_device:
-                print(v.devices[device].get_serial(),position_device.get_position_x(),position_device.get_position_y())
-                if position_device.get_position_x()[0] > 0 and position_device.get_position_y()[0] > 0.3 and position_device.get_position_y()[0] < 1:
-                    right_knee = (v.devices[device].get_serial(),v.device_index_map[v.devices[device].index])
-                if position_device.get_position_x()[0] < 0 and position_device.get_position_y()[0] > 0.3 and position_device.get_position_y()[0] < 1:
-                    left_knee = (v.devices[device].get_serial(),v.device_index_map[v.devices[device].index])
+                #print(v.devices[device].get_serial(), position_device.get_position_x(),
+                #      position_device.get_position_y())
+                if position_device.get_position_x()[0] > 0 and position_device.get_position_y()[0] > 0.3 and \
+                        position_device.get_position_y()[0] < 1:
+                    right_knee = (v.devices[device].get_serial(), v.device_index_map[v.devices[device].index])
+                if position_device.get_position_x()[0] < 0 and position_device.get_position_y()[0] > 0.3 and \
+                        position_device.get_position_y()[0] < 1:
+                    left_knee = (v.devices[device].get_serial(), v.device_index_map[v.devices[device].index])
                 if position_device.get_position_x()[0] > 0 and position_device.get_position_y()[0] < 0.3:
-                    right_leg = (v.devices[device].get_serial(),v.device_index_map[v.devices[device].index])
+                    right_leg = (v.devices[device].get_serial(), v.device_index_map[v.devices[device].index])
                 if position_device.get_position_x()[0] < 0 and position_device.get_position_y()[0] < 0.3:
-                    left_leg = (v.devices[device].get_serial(),v.device_index_map[v.devices[device].index])
+                    left_leg = (v.devices[device].get_serial(), v.device_index_map[v.devices[device].index])
                 if position_device.get_position_x()[0] > 0 and position_device.get_position_y()[0] > 1:
-                    right_hand = (v.devices[device].get_serial(),v.device_index_map[v.devices[device].index])
+                    right_hand = (v.devices[device].get_serial(), v.device_index_map[v.devices[device].index])
                 if position_device.get_position_x()[0] < 0 and position_device.get_position_y()[0] > 1:
-                    left_hand = (v.devices[device].get_serial(),v.device_index_map[v.devices[device].index])
+                    left_hand = (v.devices[device].get_serial(), v.device_index_map[v.devices[device].index])
                 self.slovar_trackers = {"Правое_колено": right_knee,
                                         "Левое_колено": left_knee,
                                         "Правая_голень": right_leg,
@@ -61,22 +69,22 @@ class Get_data_trackers():
                                         "Правая_перчатка": right_hand,
                                         "Левая_перчатка": left_hand}
 
-
     def get_info(self):
         v = triad_openvr.triad_openvr()
         v.print_discovered_objects()
         n = 0
+        a = 0
         data = []
-        print(self.slovar_trackers)
-        while n<5:
+        #print(self.slovar_trackers)
+        while n < 300:
 
             data_current = []
             for serial in self.slovar_trackers:
                 if self.slovar_trackers[serial]:
                     try:
-                        print(self.slovar_trackers[serial])
+                        #print(self.slovar_trackers[serial])
                         current_serial, device = self.slovar_trackers[serial]
-                        position_device = v.devices[device].sample(1, 50)
+                        position_device = v.devices[device].sample(1, 20)
 
                         if position_device:
                             c = position_device.get_position()
@@ -84,20 +92,26 @@ class Get_data_trackers():
                                     current_serial != 'LHR-1A2114EA':
                                 '''Get_data_trackers.csv_writer('p.csv', Get_data_trackers.fieldnames, 
                                                              position_device.get_position())'''  # 1 Вариант записи в csv
-                                data_current.extend([c[0][0],c[1][0],c[2][0]])
+                                data_current.extend([c[0][0], c[1][0], c[2][0]])
 
                     except Exception as e:
                         print(e)
-                        #if n>0:
+                        # if n>0:
                         #    data_current.append(data_current[n - 1][num_device])
 
             data_current.append(CURRENT_ACTION)
             data.append(data_current)
-            n +=1
-        print(data)
-        self.csv_writer('p.csv', self.fieldnames,
-                                     data)  # 2 Вариант записи в csv
+            n += 1
 
+        '''while a < 100:
+            a += 1
+            print(data)
+            self.csv_writer('p.csv', self.fieldnames,
+                            data)  # 2 Вариант записи в csv
+
+        print(data)'''
+        self.csv_writer('p.csv', self.fieldnames,
+                            data)
 
 if __name__ == '__main__':
     ex = Get_data_trackers()
