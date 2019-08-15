d4e78d12d734e71e782676af14fffebab10b7e43
Андрей <45171006+ProGlot003@users.noreply.github.com>
2019-08-15 21:30:55 +0300

    Add files via upload
    Чистый репозиторий

---
+++ Нужные файлы/testSlider.py
@@ -0,0 +1,285 @@
+import sys
+from PyQt5 import uic
+from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtWidgets import QInputDialog
+from PyQt5.QtCore import Qt, QThread, pyqtSignal
+from PyQt5.QtWidgets import (QSlider, QApplication, QVBoxLayout)
+import sys
+import serial
+import time
+import serial.tools.list_ports
+import glob
+import socket
+
+current_port = ''
+current_speed = ''
+flag_port = False
+flag_speed = False
+flag_start = False
+flag_stop = True
+flag_reload = False
+flag_increase_speed = False
+stop_speed = 0
+x = 0
+data_in_arduino = 0
+print('Loading...')
+
+UDP_PORT = 5005
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.connect(('localhost', UDP_PORT))
+
+
+def Search(__baudrate=115200, timeSleep=5):
+    speed_signal = pyqtSignal(bytes)
+
+    # Port Database
+    __COMlist = []
+    __COM = ['COM' + str(i) for i in range(2, 100)]
+
+    for _COM in __COM:
+        try:
+            COMport = (serial.Serial(port=_COM, \
+                                     baudrate=__baudrate, \
+                                     parity=serial.PARITY_NONE, \
+                                     stopbits=serial.STOPBITS_ONE, \
+                                     bytesize=serial.EIGHTBITS, \
+                                     timeout=0))
+
+            if COMport:
+                # COMlist Creation
+                __COMlist.append(_COM)
+            else:
+                pass
+
+        except Exception as e:
+            '''ErrorAttachment = open("SerialErrorAttachment.txt", "a")
+            ErrorAttachment.write(e.__class__.__name__ + "\r")
+            ErrorAttachment.close()'''
+            continue
+    return __COMlist
+
+
+def CheckSerialPortMessage(__activeCOM=Search(), __baudrate=115200, __timeSleep=5):
+    try:
+        for __COM in __activeCOM:
+
+            port = serial.Serial(__COM, __baudrate)
+            time.sleep(__timeSleep)
+            large = len(port.readline())
+            port.read(large)
+
+            while True:
+
+                if large > 3:
+                    for a in range(__timeSleep):
+
+                        date = port.readline().decode().split()
+
+                        if 'treadmill' in date:
+                            return __COM
+
+                else:
+                    break
+
+    except Exception as e:
+        return e
+
+
+CheckSerialPortMessage()
+
+
+class SerialThread(QThread):
+    def __init__(self):
+        global current_speed, current_port
+        super().__init__()
+        self.speed = 0
+        self.speed2 = 0
+        port = serial.Serial(current_port, current_speed)
+        self.port = port
+        self.rt = None
+        self.start()
+
+    def run(self):
+        while True:
+            difference = self.speed2 - self.speed
+            if difference >= 40:
+                self.speed += 2
+            if difference >= 20 and difference < 40:
+                self.speed += 1
+            if difference < 20 and difference > 0:
+                self.speed += 1
+            if difference <= -40:
+                self.speed -= 2
+            if difference <= -20 and difference > -40:
+                self.speed -= 1
+            if difference > -20 and difference < 0:
+                self.speed -= 1
+            if self.speed > self.speed2:
+                self.speed -= 1
+            self.write_to_port()
+            time.sleep(0.1)
+
+    def stop(self):
+        if self.rt:
+            self.rt.join()
+
+    def write_to_port(self):
+        if flag_stop:
+            x = str(self.speed) + '.'
+        if not flag_stop:
+            if self.speed != 0:
+                self.speed -= 1
+                self.speed2 = self.speed
+                x = str(self.speed) + '.'
+            else:
+                x = str(0) + '.'
+        self.port.write(bytes(x, 'utf-8'))
+        a = x.replace('.', '')
+        sock.send(bytes(str(a), 'utf-8'))
+        print(self.port.readline())
+
+
+class Sliderdemo(QMainWindow):
+    speed_signal = pyqtSignal(bytes)
+
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('int.ui', self)
+        self.initUI()
+
+    def initUI(self):
+        global stop_speed
+        self.start.clicked.connect(self.button_start)
+        self.reload.clicked.connect(self.button_reload)
+        self.current_speed_ports.clicked.connect(self.transform_selection)
+        self.current_COM_port.clicked.connect(self.get_dialog)
+        self.pushButton.clicked.connect(self.button_connection)
+        self.stop.clicked.connect(self.button_stop)
+        self.setWindowTitle("Тестовый режим")
+        self.result.display(0)
+        self.text_terminal.setText(
+            "       Проверка подключения")
+        self.sld = QSlider(Qt.Horizontal, self)
+        self.sld.setGeometry(250, 330, 160, 22)
+        self.sld.setMinimum(0)
+        self.sld.setTickInterval(1)
+        self.sld.setMaximum(255)
+        self.sld.setValue(0)
+        self.sld.setTickPosition(QSlider.TicksBelow)
+        self.sld.setTickInterval(10)
+        self.sld.valueChanged[int].connect(self.valuechange)
+        self.sld.valueChanged.connect(self.result.display)
+        self.sld.setEnabled(False)
+        self.reload.setEnabled(False)
+        vbox = QVBoxLayout()
+        vbox.addWidget(self.result)
+        vbox.addWidget(self.sld)
+        self.show()
+
+    def get_dialog(self):
+        global current_port, flag_port
+        current_port, flag_port = QInputDialog.getItem(self, "Выберите COM-порт",
+                                                       "Доступные COM-Порты",
+                                                       Search(), 0, False)
+        return current_port
+
+    def transform_selection(self):
+        global current_speed, flag_speed
+        speeds = ['1200', '2400', '4800', '9600', '19200', '38400', '57600', '115200']
+        current_speed, flag_speed = QInputDialog.getItem(self, "Выберите скорость", "Доступные скорости",
+                                                         speeds, 0, False)
+        return current_speed
+
+    def button_connection(self):
+        global current_speed, current_port, flag_speed, flag_port, flag_start
+        if flag_speed and flag_port:
+            try:
+                flag_start = True
+
+                self.pushButton.setText('Подключено')
+                self.text_terminal.setText(
+                    "Порт, к которому вы подключились: " + current_port +
+                    " Скорость порта, которую вы выбрали: " + current_speed)
+            except Exception as e:
+                self.pushButton.setText(e)
+                print(e)
+        else:
+            if not flag_port and flag_speed:
+                self.text_terminal.setText(
+                    "Вы не выбрали порт, повторите попытку.")
+            if not flag_speed and flag_port:
+                self.text_terminal.setText(
+                    "Вы не выбрали скорость порта, повторите попытку.")
+            if not flag_speed and not flag_port:
+                self.text_terminal.setText(
+                    "Вы не выбрали ни порт, ни скорость порта. Выберите нужный вам и порт и скорость этого порта. Поторите попытку.")
+
+    def on_progress(self, value):
+        print(value)
+
+    def on_finished(self):
+        self.thread.progressed.disconnect(self.on_progress)
+        self.thread.finished.disconnect(self.on_finished)
+        self.thread = None
+
+    def valuechange(self, value):
+        self.speed = value
+        # print("__init__vSl -> ", self.speed)
+        self.thread.speed2 = self.speed
+        return self.size
+
+    def button_start(self):
+        global flag_start, flag_increase_speed
+        if flag_start:
+            self.sld.setEnabled(True)
+            self.start.setEnabled(False)
+            self.current_speed_ports.setEnabled(False)
+            self.current_COM_port.setEnabled(False)
+            self.pushButton.setEnabled(False)
+            self.thread = SerialThread()
+            flag_increase_speed = True
+            self.thread.start()
+            if not self.thread:
+                self.thread.progressed.connect(self.on_progress)
+                self.thread.finished.connect(self.on_finished)
+                flag_increase_speed = True
+                self.thread.start()
+
+        else:
+            self.text_terminal.setText(
+                "Вы не выбрали либо порт, либо скорость порта, повторите попытку.")
+
+    def button_stop(self):
+        self.result.display(0)
+        self.sld.setValue(0)
+        global x, flag_stop
+        self.stop.setEnabled(False)
+        if flag_start:
+            flag_stop = False
+            self.sld.setEnabled(False)
+            self.reload.setEnabled(True)
+
+            # self.sld.setValue(0)
+            # self.result.display(0)
+            # self.thread.stop()
+            # sys.exit(app.exec_())
+        else:
+            self.text_terminal.setText(
+                "Данных на arduino пока не поступало.")
+
+    def button_reload(self):
+        global flag_start, flag_stop, flag_reload
+        flag_reload = True
+        self.stop.setEnabled(True)
+        flag_stop = True
+        self.result.display(0)
+        self.speed = 0
+        self.sld.setValue(0)
+        self.reload.setEnabled(False)
+        self.sld.setEnabled(True)
+
+
+app = QApplication(sys.argv)
+ex = Sliderdemo()
+ex.show()
+sys.exit(app.exec_())
---
+++ Нужные файлы/tracker_test.py
@@ -0,0 +1,86 @@
+import triad_openvr
+import time
+import sys
+import csv
+
+
+def csv_dict_writer(path, fieldnames, data):
+    """
+    Writes a CSV file using DictWriter
+    """
+    with open(path, "w", newline='') as out_file:
+        writer = csv.writer(out_file, delimiter=';')
+        writer.writerow(fieldnames)
+        for row in data:
+            writer.writerow(row)
+
+
+trackers = {'LHR-3A018118': ["tracker_4", 3],
+            'LHR-9224071E': ["tracker_3", 2],
+            'LHR-89FBFC40': ["tracker_2", 1],
+            'LHR-1761CD18': ["tracker_1", 0]}
+print(trackers.keys())
+v = triad_openvr.triad_openvr()
+v.print_discovered_objects()
+data = []
+if len(sys.argv) == 1:
+    interval = 1 / 500
+elif len(sys.argv) == 2:
+    interval = 1 / float(sys.argv[1])
+else:
+    print("Invalid number of arguments")
+    interval = False
+
+n = 0
+if interval:
+    while (n < 20):
+        start = time.time()
+        txt = ""
+        data_current = []
+        for serial in trackers.keys():
+            device, num_device = trackers[serial]
+            try:
+
+                '''print(device)'''
+                position_device = v.devices[device].sample(1, 500)
+                if position_device and n > 0:
+                    if v.devices[device].get_serial() != 'LHR-3A018118' and v.devices[
+                        device].get_serial() != 'LHR-1A2114EA':
+                        print(v.devices[device].get_need_serial())
+                        print(position_device.get_position())
+                        # переделать пришедшие данные - если по 1 х у z, то сразу в data_current
+                        # переделать пришедшие данные - если пришло много, то сразу в data
+                        data_current.append(position_device.get_position())
+                """
+                for each in v.devices[device].get_pose_euler():
+                    txt += "%.4f" % each
+                    txt += " "
+                print("\r" + txt, end="")"""
+                '''print()'''
+            except Exception as e:
+                if n > 0:
+                    data_current.append(data_current[n - 1][num_device])
+                    pass
+        data.append(data_current)
+        sleep_time = interval - (time.time() - start)
+        if sleep_time > 0:
+            time.sleep(sleep_time)
+        n += 1
+print(len(data))
+print(data)
+fieldnames = ["tracker_1_x",
+              "tracker_1_y",
+              "tracker_1_z",
+              "tracker_2_x",
+              "tracker_2_y",
+              "tracker_2_z",
+              "tracker_3_x",
+              "tracker_3_y",
+              "tracker_3_z",
+              "tracker_4_x",
+              "tracker_4_y",
+              "tracker_4_z"]
+data = [[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+        [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+        [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], ]
+csv_dict_writer('p.csv', fieldnames, data)
