83aa51e78d52e4d39ed15a839ce83c1acd11161f
Artem <obuhov.art@gmail.com>
2019-08-20 15:08:58 +0300

    нейронная сеть для распознавания движения

---
+++ Нужные файлы/NN.py
@@ -0,0 +1,47 @@
+from keras.layers import *
+from keras.preprocessing.text import *
+from keras import Sequential
+from string import ascii_letters
+from keras.utils.np_utils import to_categorical as to_cat
+from sklearn.model_selection import train_test_split as t_t_split
+import pandas
+import random
+import numpy as np
+
+data = pandas.read_csv("data.csv", sep=";")
+
+
+X = data.values[:,  1:6:3]
+X_new = np.asarray([data.values[i:i+10, 1:6:3] for i in range(len(X) - 10 )])
+Y = data.values[:5990,  6:7]
+print(X_new.shape, X_new[5000],Y[5000])
+X = X_new
+print(Y.shape, Y[0])
+#variant 3
+"""
+
+X = data[:, 0:6]
+X = X.reshape((-1, 10, 6))
+Y = data[1:61, 0:6]
+Y = Y.reshape((-1, 10, 6))
+print(Y)
+"""
+X_train, Y_train, X_test, Y_test = X[:5000],Y[:5000],X[5000:],Y[5000:5990]
+model = Sequential()
+model.add(Dense(100, activation="relu", input_shape=(10, 2,)))
+model.add(Flatten())
+model.add(Dense(50, activation="relu"))
+model.add(Dropout(0.2))
+model.add(Dense(100, activation="relu"))
+model.add(Dense(10, activation="relu"))
+model.add(Dense(1, activation="relu"))
+model.compile(optimizer='adam', loss='mse', metrics=['accuracy'])
+model.summary()
+model.fit(X, Y, epochs=20, batch_size=100, validation_split=0.2, shuffle=True)
+score = model.evaluate(X_test, Y_test)
+print(score)
+
+
+import time
+name_time = str(time.time())[-4:-1]
+model.save("NN_treadmill"+name_time+".h5")
---
+++ Нужные файлы/NN_test.py
@@ -0,0 +1,23 @@
+from keras.layers import *
+from keras.preprocessing.text import *
+from keras import Sequential
+from string import ascii_letters
+from keras.utils.np_utils import to_categorical as to_cat
+from sklearn.model_selection import train_test_split as t_t_split
+import pandas
+import random
+import numpy as np
+
+from keras.models import load_model
+data = pandas.read_csv("data.csv", sep=";")
+
+
+X = data.values[:,  1:6:3]
+X_new = np.asarray([data.values[i:i+10, 1:6:3] for i in range(len(X) - 10 )])
+Y = data.values[:5990,  6:7]
+X = X_new
+
+NN = load_model("NN_treadmill574.h5")
+new_data = NN.predict(X)
+for i in range(len(X)):
+    print(new_data[i],Y[i])
